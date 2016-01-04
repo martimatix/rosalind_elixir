65342061d9bc9499dadaad86cf718fe6371a9ca1
Mario Martinez <zenitram.oiram@gmail.com>
2016-01-04 22:56:05 +1100

    Update wrapping in problem descriptions

--- 002_rna.exs
+++ 002_rna.exs
@@ -1,8 +1,10 @@
 # Problem
 #
-# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and 'U'.
+# An RNA string is a string formed from the alphabet containing 'A', 'C', 'G', and
+# 'U'.
 #
-# Given a DNA string tt corresponding to a coding strand, its transcribed RNA string uu is formed by replacing all occurrences of 'T' in tt with 'U' in uu.
+# Given a DNA string tt corresponding to a coding strand, its transcribed RNA
+# string uu is formed by replacing all occurrences of 'T' in tt with 'U' in uu.
 #
 # Given: A DNA string tt having length at most 1000 nt.
 #
@@ -10,8 +12,7 @@
 #
 # Sample Dataset
 #
-# GATGGAACTTGACTACGTAAATT
-# Sample Output
+# GATGGAACTTGACTACGTAAATT Sample Output
 #
 # GAUGGAACUUGACUACGUAAAUU
 
