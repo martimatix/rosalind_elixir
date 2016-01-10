5f5069583589bd9b481fd8b055d9e7c0f1ca53ce
Mario Martinez <zenitram.oiram@gmail.com>
2016-01-10 19:55:45 +1100

    Add struct to fasta module

--- fasta_module.exs
+++ fasta_module.exs
@@ -1,6 +1,7 @@
 # Module for parsing FASTA files
 
 defmodule Fasta do
+  defstruct id: "FASTA", value: ""
 
   # Returns a list of tuples - first element is the FASTA label
   # and second is the FASTA string
@@ -17,24 +18,16 @@ defmodule Fasta do
       |> List.first
   end
 
-  # Returns a map with the FASTA label as a key and
-  # the FASTA string as the value
-  def read(filename, :map) do
-    read(file)
-      |> Enum.into(%{})
-  end
-
   def compile(lines) do
     Enum.reduce(lines, [], fn(line, acc) ->
       case String.first(line) do
         ">" ->
           # Remove ">" character from label
-          fasta_label = String.lstrip(line, ?>)
-          acc = [ {fasta_label, ""} | acc ]
+          fasta_id = String.lstrip(line, ?>)
+          acc = [ %Fasta{id: fasta_id} | acc ]
         _ ->
-          [ tuple | tail ] = acc
-          fasta_value = elem(tuple, 1) <> line
-          acc = [ put_elem(tuple, 1, fasta_value) | tail ]
+          [ fasta | tail ] = acc
+          acc = [ %{fasta | value: fasta.value <> line} | tail ]
       end
     end)
   end
