865bf483faf01b496f365636623eb1d6ba75d56c
Mario Martinez <zenitram.oiram@gmail.com>
2016-01-25 20:05:13 +1100

    Add solution for PROT

---
+++ 008_prot.exs
@@ -0,0 +1,26 @@
+# Problem
+#
+# The 20 commonly occurring amino acids are abbreviated by using 20 letters from
+# the English alphabet (all letters except for B, J, O, U, X, and Z). Protein
+# strings are constructed from these 20 symbols. Henceforth, the term genetic
+# string will incorporate protein strings along with DNA strings and RNA strings.
+#
+# The RNA codon table dictates the details regarding the encoding of specific
+# codons into the amino acid alphabet.
+#
+# Given: An RNA string ss corresponding to a strand of mRNA (of length at most 10
+# kbp).
+#
+# Return: The protein string encoded by ss.
+#
+# Sample Dataset
+# AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA
+#
+# Sample Output
+# MAMAPRTEINSTRING
+
+Code.require_file "rosalind_module.exs", __DIR__
+
+rna = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+
+IO.puts Rosalind.rna_to_protein(rna)
--- rosalind_module.exs
+++ rosalind_module.exs
@@ -1,6 +1,25 @@
 # Module for shared functions used in Rosalind problems
 
 defmodule Rosalind do
+  @rna_codon_table %{
+    'UUU' => 'F',     'CUU' => 'L',      'AUU' => 'I',      'GUU' => 'V',
+    'UUC' => 'F',     'CUC' => 'L',      'AUC' => 'I',      'GUC' => 'V',
+    'UUA' => 'L',     'CUA' => 'L',      'AUA' => 'I',      'GUA' => 'V',
+    'UUG' => 'L',     'CUG' => 'L',      'AUG' => 'M',      'GUG' => 'V',
+    'UCU' => 'S',     'CCU' => 'P',      'ACU' => 'T',      'GCU' => 'A',
+    'UCC' => 'S',     'CCC' => 'P',      'ACC' => 'T',      'GCC' => 'A',
+    'UCA' => 'S',     'CCA' => 'P',      'ACA' => 'T',      'GCA' => 'A',
+    'UCG' => 'S',     'CCG' => 'P',      'ACG' => 'T',      'GCG' => 'A',
+    'UAU' => 'Y',     'CAU' => 'H',      'AAU' => 'N',      'GAU' => 'D',
+    'UAC' => 'Y',     'CAC' => 'H',      'AAC' => 'N',      'GAC' => 'D',
+    'UAA' => :Stop,   'CAA' => 'Q',      'AAA' => 'K',      'GAA' => 'E',
+    'UAG' => :Stop,   'CAG' => 'Q',      'AAG' => 'K',      'GAG' => 'E',
+    'UGU' => 'C',     'CGU' => 'R',      'AGU' => 'S',      'GGU' => 'G',
+    'UGC' => 'C',     'CGC' => 'R',      'AGC' => 'S',      'GGC' => 'G',
+    'UGA' => :Stop,   'CGA' => 'R',      'AGA' => 'R',      'GGA' => 'G',
+    'UGG' => 'W',     'CGG' => 'R',      'AGG' => 'R',      'GGG' => 'G',
+  }
+
   def dna_to_rna(dna), do: String.replace(dna, "T", "U")
 
   def complement(symbol) do
@@ -17,5 +36,13 @@ defmodule Rosalind do
                 |> String.codepoints
                 |> Enum.count(&(&1 == "G" || &1 == "C"))
     gc_count / String.length(dna) * 100
+  end
+
+  def rna_to_protein(rna) do
+    rna
+      |> Stream.chunk(3)
+      |> Stream.map(&(@rna_codon_table[&1]))
+      |> Stream.take_while(&(&1 != :Stop))
+      |> Enum.to_list
   end
 end
